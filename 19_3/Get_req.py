6f42ef7baff22b93585254e231ab21ceea8c641f
Kluck911 <kluck1812@gmail.com>
2021-11-20 13:29:29 +0300

    19_3_get_req

---
+++ 19_3/Get_req.py
@@ -0,0 +1,21 @@
+import json.decoder
+
+import requests
+
+
+def get_api_key(email: str, passwd: str):
+    headers = {
+        'email': email,
+        'password': passwd,
+    }
+    res = requests.get('https://petfriends1.herokuapp.com/api/key', headers=headers)
+    status = res.status_code
+
+    try:
+        result = res.json()
+    except json.decoder.JSONDecodeError:
+        result = res.text
+    return status, result
+
+
+print(get_api_key('kluck1812@gmail.com', 'klicKKluck1812'))
